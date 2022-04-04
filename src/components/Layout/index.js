9fa7f990f2c0a18e3f6791e1345cfd4994083d15
midCenturyDude7 <mgriffes@gmail.com>
2022-04-04 08:08:39 -0700

    Adding navbar with icons and links

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Sidebar from "../Sidebar";
 import "./index.scss";
 
 const Layout = () => {
-  return <>Hello</>;
+  return (
+    <Sidebar />
+  );
 };
 
 export default Layout;
