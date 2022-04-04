4a6510fa9dbd2a5b8fddc13857cc498fdb79e28b
midCenturyDude7 <mgriffes@gmail.com>
2022-04-03 22:22:52 -0700

    Initializing app config and structure

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: [
+    "react"
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    // we want to force semicolons
+    semi: ["error", "always"],
+    // we use 2 spaces to indent our code
+    indent: ["error", 2],
+    // we want to avoid extraneous spaces
+    "no-multi-spaces": ["error"]
+  }
+};
