fc5edae968b5f3c4c265f872b77aace9e2a5f50c
midCenturyDude7 <mgriffes@gmail.com>
2022-04-14 19:49:26 -0700

    Updated project page layout

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Loader from "react-loaders";
 import "./index.scss";
+// import LoginApp from "../../assets/images/login_app.png";
 import AnimatedLetters from "../AnimatedLetters";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 // const logo = document.querySelectorAll("#logo path");
 // for (let i = 0; i < logo.length; i++) {
@@ -43,12 +47,38 @@ const Work = () => {
           <h1>Some things I&apos;ve built:</h1>
         </div>
         <div className="grid-content-container-right">
-          <div className="grid-content-card">Card One - a screenshot image</div>
-          <div className="grid-content-card"></div>
-          <div className="grid-content-card">A simple login application that allows the user
-            to create a profile page and upload an avatar.
+          <div className="grid-content-card">
+            [Placeholder for the image]
+            {/* <img src={LoginApp} alt="Login App" /> */}
           </div>
-          <div className="grid-content-card"></div>
+          <div className="grid-content-card" id="header">
+            <h5>Featured project</h5>
+            <h3>Login Application</h3>
+          </div>
+          <div className="grid-content-card">
+            <span>A simple login application that allows the user
+            to create a profile page and upload an avatar.</span>
+          </div>
+          <div className="grid-content-card">
+            <div>
+              <span>Next.js</span>
+              <span>React</span>
+              <span>Chakra-UI</span>
+              <span>Supabase</span>
+            </div>
+            <div className="grid-links">
+              <span>
+                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </span>
+              <span>
+                <a target="_blank" rel="noreferrer" href="#">
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                </a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
 
