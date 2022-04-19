d0f29b6673f09dc544eeb911f78eea6842131130
midCenturyDude7 <mgriffes@gmail.com>
2022-04-18 17:57:42 -0700

    Updated project page to include three projects w/mag layout

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -43,47 +43,6 @@ const Work = () => {
           </h1>
         </div>
       </div>
-      {/* <div className="grid-container">
-        <div className="grid-header">
-          <h1>RECENT PROJECTS</h1>
-        </div>
-        <div className="grid-content-container-right">
-          <div className="grid-content-card">
-            <a href="#"><img src={LoginApp} alt="Login App" /></a>
-          </div>
-          <div className="grid-content-card" id="header">
-            <h5>Featured project</h5>
-            <h3>LOGIN APPLICATION</h3>
-          </div>
-          <div className="grid-content-card">
-            <span>A simple login application that allows the user
-            to create a profile page and upload an avatar.</span>
-          </div>
-          <div className="grid-content-card">
-            <div>
-              <span>Next.js</span>
-              <span>React</span>
-              <span>Chakra-UI</span>
-              <span>Supabase</span>
-            </div>
-            <div className="grid-links">
-              <span>
-                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7">
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </span>
-              <span>
-                <a target="_blank" rel="noreferrer" href="#">
-                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </a>
-              </span>
-              <div className="grid-button">
-                <Link to="#" className="button">CASE STUDY</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className="wrapper">
         <div className="grid-header">
           <h1>RECENT PROJECTS</h1>
@@ -91,7 +50,6 @@ const Work = () => {
         <div className='grid-container-right'>
           <div className='grid-item-right'>
             <a href="#"><img src={LoginApp} alt="Login App" /></a>
-
           </div>
           <div className='grid-item-right'>
             <h5>Featured Project</h5>
@@ -110,7 +68,7 @@ const Work = () => {
             </div>
             <div className="grid-links">
               <span>
-                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7">
+                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7/next-login-chakra-ui-prototype">
                   <FontAwesomeIcon icon={faGithub} />
                 </a>
               </span>
@@ -127,14 +85,86 @@ const Work = () => {
             </div>
           </div>
         </div>
-        {/* <div className='grid-container-left'>
-          <div className='grid-item-left'>BOX 1</div>
-          <div className='grid-item-left'>BOX 2</div>
-          <div className='grid-item-left'>BOX 3</div>
-          <div className='grid-item-left'>BOX 4</div>
-          <div className='grid-item-left'>BOX 5</div>
+        <div className='grid-container-left'>
+          <div className='grid-item-left'>
+            <h5>Featured Project</h5>
+            <h3>SPOTIFY API APPLICATION</h3>
+          </div>
+          <div className='grid-item-left'>
+            <div className='grid-item-right'>
+              <a href="#"><img src={LoginApp} alt="Login App" /></a>
+            </div>
+          </div>
+          <div className='grid-item-left'>
+            <span>A simple login application that allows the user
+            to create a profile page and upload an avatar.</span>
+          </div>
+          <div className='grid-item-left'>
+            <div>
+              <span>Express</span>
+              <span>React</span>
+              <span>CSS</span>
+              <span>Heroku</span>
+            </div>
+            <div className="grid-links">
+              <span>
+                <a target="_blank" rel="noreferrer" href="#">
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                </a>
+              </span>
+              <span>
+                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7/nodejs-react-spotify-app">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className='grid-item-left'>
+            <div className="grid-button">
+              <Link to="#" className="button">CASE STUDY</Link>
+            </div>
+          </div>
         </div>
         <div className='grid-container-right'>
+          <div className='grid-item-right'>
+            <a href="#"><img src={LoginApp} alt="Login App" /></a>
+          </div>
+          <div className='grid-item-right'>
+            <h5>Featured Project</h5>
+            <h3>PORTFOLIO WEBSITE</h3>
+          </div>
+          <div className='grid-item-right'>
+            <span>A simple login application that allows the user
+            to create a profile page and upload an avatar.</span>
+          </div>
+          <div className='grid-item-right'>
+            <div>
+              <span>React</span>
+              <span>Sass</span>
+              <span>Netlify</span>
+              <span></span>
+            </div>
+            <div className="grid-links">
+              <span>
+                <a target="_blank" rel="noreferrer" href="https://github.com/midcenturydude7/react-portfolio-animations-app">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </span>
+              <span>
+                <a target="_blank" rel="noreferrer" href="#">
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className='grid-item-right'>
+            <div className="grid-button">
+              <Link to="#" className="button">CASE STUDY</Link>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className='grid-container-right'>
           <div className='grid-item-right'>BOX 1</div>
           <div className='grid-item-right'>BOX 2</div>
           <div className='grid-item-right'>BOX 3</div>
